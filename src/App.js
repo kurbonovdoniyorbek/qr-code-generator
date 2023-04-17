c506d0ad31972c611b1c44d1af17c94885587a32
Mac Book Pro <macbookpro@doniyojon.local>
2023-04-18 02:14:24 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [secondclass, setSecondclass] = useState(false)
+  const [inputvalue, setInputvalue] = useState('')
+  const image_link = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputvalue}`
+
+
+  const myFunc = () => {
+    setSecondclass(secondclass => !secondclass)
+  }
+
+  const handleKeyUp = () => {
+    if (!inputvalue) {
+      setSecondclass(false)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hh">
+      <div className={`wrapper ${secondclass ? "active" : ""}`}>
+        <header>
+          <h1>QR Code Generator</h1>
+          <p>paste a url or enter text to create QR code</p>
+        </header>
+        <div className='form'>
+          <input value={inputvalue} onChange={(e) => setInputvalue(e.target.value)} type="text" placeholder='Enter text or Url' onKeyUp={handleKeyUp} />
+          <button onClick={myFunc}>Generate QR Code</button>
+        </div>
+        <div className="qr-code">
+          <img src={image_link} alt="qr-code" />
+        </div>
+      </div>
+      <p className='name'>Created by <h1>Doniyorjon</h1></p>
     </div>
   );
 }
